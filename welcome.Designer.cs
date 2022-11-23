7a2c15745a58fcc162d495e56d56c562306e815b
Heli kheni <helipatel56789@gmail.com>
2022-11-22 20:22:04 -0500

    try

--- welcome.Designer.cs
+++ welcome.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(welcome));
             this.label1 = new System.Windows.Forms.Label();
             this.btnDine = new System.Windows.Forms.Button();
             this.btnAdmin = new System.Windows.Forms.Button();
@@ -44,9 +43,10 @@
             this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.Yellow;
-            this.label1.Location = new System.Drawing.Point(151, 21);
+            this.label1.Location = new System.Drawing.Point(121, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(478, 60);
+            this.label1.Size = new System.Drawing.Size(403, 51);
             this.label1.TabIndex = 0;
             this.label1.Text = "Welcome to IndianTaste";
             // 
@@ -54,9 +54,10 @@
             // 
             this.btnDine.BackColor = System.Drawing.Color.Olive;
             this.btnDine.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnDine.Location = new System.Drawing.Point(180, 151);
+            this.btnDine.Location = new System.Drawing.Point(144, 121);
+            this.btnDine.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDine.Name = "btnDine";
-            this.btnDine.Size = new System.Drawing.Size(171, 65);
+            this.btnDine.Size = new System.Drawing.Size(137, 52);
             this.btnDine.TabIndex = 1;
             this.btnDine.Text = "DINE IN";
             this.btnDine.UseVisualStyleBackColor = false;
@@ -66,9 +67,10 @@
             // 
             this.btnAdmin.BackColor = System.Drawing.Color.Olive;
             this.btnAdmin.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnAdmin.Location = new System.Drawing.Point(180, 273);
+            this.btnAdmin.Location = new System.Drawing.Point(144, 218);
+            this.btnAdmin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnAdmin.Name = "btnAdmin";
-            this.btnAdmin.Size = new System.Drawing.Size(171, 63);
+            this.btnAdmin.Size = new System.Drawing.Size(137, 50);
             this.btnAdmin.TabIndex = 2;
             this.btnAdmin.Text = "ADMIN ";
             this.btnAdmin.UseVisualStyleBackColor = false;
@@ -77,9 +79,10 @@
             // 
             this.btnEmp.BackColor = System.Drawing.Color.Olive;
             this.btnEmp.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnEmp.Location = new System.Drawing.Point(375, 387);
+            this.btnEmp.Location = new System.Drawing.Point(300, 310);
+            this.btnEmp.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnEmp.Name = "btnEmp";
-            this.btnEmp.Size = new System.Drawing.Size(202, 69);
+            this.btnEmp.Size = new System.Drawing.Size(162, 55);
             this.btnEmp.TabIndex = 3;
             this.btnEmp.Text = "EMPLOYEE";
             this.btnEmp.UseVisualStyleBackColor = false;
@@ -89,9 +92,10 @@
             // 
             this.btnCash.BackColor = System.Drawing.Color.Olive;
             this.btnCash.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnCash.Location = new System.Drawing.Point(375, 146);
+            this.btnCash.Location = new System.Drawing.Point(300, 117);
+            this.btnCash.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCash.Name = "btnCash";
-            this.btnCash.Size = new System.Drawing.Size(202, 70);
+            this.btnCash.Size = new System.Drawing.Size(162, 56);
             this.btnCash.TabIndex = 4;
             this.btnCash.Text = "CASH OUT";
             this.btnCash.UseVisualStyleBackColor = false;
@@ -101,9 +105,10 @@
             // 
             this.btnCashin.BackColor = System.Drawing.Color.Olive;
             this.btnCashin.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnCashin.Location = new System.Drawing.Point(375, 273);
+            this.btnCashin.Location = new System.Drawing.Point(300, 218);
+            this.btnCashin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCashin.Name = "btnCashin";
-            this.btnCashin.Size = new System.Drawing.Size(202, 63);
+            this.btnCashin.Size = new System.Drawing.Size(162, 50);
             this.btnCashin.TabIndex = 5;
             this.btnCashin.Text = "CASH IN";
             this.btnCashin.UseVisualStyleBackColor = false;
@@ -111,17 +116,17 @@
             // 
             // welcome
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(793, 557);
+            this.ClientSize = new System.Drawing.Size(634, 446);
             this.Controls.Add(this.btnCashin);
             this.Controls.Add(this.btnCash);
             this.Controls.Add(this.btnEmp);
             this.Controls.Add(this.btnAdmin);
             this.Controls.Add(this.btnDine);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "welcome";
             this.Text = "welcome";
             this.ResumeLayout(false);
